2e4bf06bf6f752631a69fd69d7e0cb6801be3c2c
Lucas <lucassanchezdl@gmail.com>
2022-10-30 01:26:27 -0300

    pasando props

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -2,20 +2,33 @@ import React from "react";
 import { TodoCounter } from "./components/TodoCounter";
 import { TodoForm } from "./components/TodoForm";
 import { TodoList } from "./components/TodoList";
+import { useTodo } from "./hooks/useTodo";
 
 export const TodoApp = () => {
+  const {
+    todoState,
+    todoCount,
+    todoPendingCount,
+    handleAddTodo,
+    handleCompleteTodo,
+    handleDeleteTodo,
+  } = useTodo();
   return (
     <div className="container">
       <h1 className="my-4">TodoApp</h1>
       <div className="row gap-2 mb-4">
-        <TodoCounter />
+        <TodoCounter count={todoCount} pendingCount={todoPendingCount} />
       </div>
       <div className="row">
         <div className="col-xs-12 col-md-5 mb-3">
-          <TodoForm />
+          <TodoForm onAddTodo={handleAddTodo} />
         </div>
         <div className="col-xs-12 col-md-7">
-          <TodoList />
+          <TodoList
+            todos={todoState}
+            onDeleteTodo={handleDeleteTodo}
+            onCompleteTodo={handleCompleteTodo}
+          />
         </div>
       </div>
     </div>
